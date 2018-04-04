5e9106abb694d3c7e10ff0f048bca50276d95a99
Matt.Lyons <matt.lyons@envisionthen.com>
2018-04-04 08:23:39 -0400

    allow static file hosting via express

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
-import * as express from 'express';
-import views from './views/routes';
-import api from './api/routes';
+import * as express from 'express'
+import views from './views/routes'
+import api from './api/routes'
 
 const port = process.env.PORT || 3000
-const app = express();
+const app = express()
 
-views.configure(app);
-api.configure(app);
+app.use(express.static('/dist/views'))
+
+views.configure(app)
+api.configure(app)
 
 app.listen(port, (err) => {
   if (err) {
